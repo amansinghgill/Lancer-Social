e22529a5e973602ae890f34f59f681ada2d52bfc
Aman Gill <gillaman1998@gmail.com>
2023-10-02 18:21:00 -0400

    First Components Created and CSS imported

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const appTitle = "Lancer Social";
+
+  return (
+    <>
+      {/* HEADER  */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            width="75"
+            height="100"
+            alt="Lancer Social Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Make a post</button>
+      </header>
+
+      <NewPostForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <PostList />
+      </main>
+    </>
+  );
+}
+
+function NewPostForm() {
+  return <form className="post-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function PostList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
